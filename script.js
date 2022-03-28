035160e880d2924b1a31611bcb83d1b6e942f3aa
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-03-28 17:14:59 -0300

    Added basic operator functionality

--- script.js
+++ script.js
@@ -1,54 +1,103 @@
 //DOM variables
-let screen = document.querySelector("#screen");
-let screenContent = screen.querySelector("span");
+const screen = document.querySelector("#screen");
+const screenTop = screen.querySelector("#top-value");
+const screenBottom = screen.querySelector("#bottom-value");
+screenBottom.textContent = 0;
+
+//Calculation variables
+let currentValue = 0;
+let operator = "";
 
 //Sets up the calculator buttons
-let allButtons = document.querySelectorAll(".calc-btn");
+const allButtons = document.querySelectorAll(".calc-btn");
 allButtons.forEach((button) => {
-  switch (button.textContent) {
-    case "=":
-      //put stuff here later
-      break;
-    case "CLEAR":
+  switch (button.id) {
+    case "btn-clear":
       button.addEventListener("click", () => {
-        screenContent.textContent = "";
+        screenBottom.textContent = 0;
+        screenTop.textContent = "";
+        currentValue = 0;
       });
       break;
-    case "DELETE":
+    case "btn-delete":
       button.addEventListener("click", () => {
-        screenContent.textContent = screenContent.textContent.slice(0, -1);
+        screenBottom.textContent = screenBottom.textContent.slice(0, -1);
       });
-
+      break;
+    case "btn-equals":
+      button.addEventListener("click", () => {
+        callOperation();
+      });
+      break;
+    case "btn-sum":
+      button.addEventListener("click", () => {
+        if (screenTop.textContent != 0 && screenBottom.textContent != 0) {
+          callOperation();
+        }
+        operator = "+";
+        screenTop.textContent = screenBottom.textContent;
+        screenBottom.textContent = 0;
+      });
+      break;
+    case "btn-subtract":
+      if (screenTop.textContent != 0 && screenBottom.textContent != 0) {
+        callOperation();
+      }
+      button.addEventListener("click", () => {
+        operator = "-";
+        screenTop.textContent = screenBottom.textContent;
+        screenBottom.textContent = 0;
+      });
+      break;
+    case "btn-multiply":
+      if (screenTop.textContent != 0 && screenBottom.textContent != 0) {
+        callOperation();
+      }
+      button.addEventListener("click", () => {
+        operator = "*";
+        screenTop.textContent = screenBottom.textContent;
+        screenBottom.textContent = 0;
+      });
+      break;
+    case "btn-divide":
+      button.addEventListener("click", () => {
+        operator = "/";
+        screenTop.textContent = screenBottom.textContent;
+        screenBottom.textContent = 0;
+      });
       break;
     default:
+      switch (button.textContent) {
+      }
       button.addEventListener("click", () => {
         console.log(button.textContent);
-        if (screenContent.textContent == "0")
-          screenContent.textContent = button.textContent;
-        else screenContent.textContent += button.textContent;
+        if (screenBottom.textContent == "0")
+          screenBottom.textContent = button.textContent;
+        else if (screenBottom.textContent.length < 18)
+          screenBottom.textContent += button.textContent;
       });
   }
 });
 
 //Operator functions
 function add(num1, num2) {
-  return num1 + num2;
+  return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
-  return num1 - num2;
+  return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2) {
-  return num1 * num2;
+  return Number(num1) * Number(num2);
 }
 
 function divide(num1, num2) {
-  return num1 / num2;
+  return Number(num1) / Number(num2);
 }
 
 function modulo(num1, num2) {
-  return num1 % num2;
+  return Number(num1) % Number(num2);
 }
 
 function operate(operator, num1, num2) {
@@ -69,4 +118,14 @@ function operate(operator, num1, num2) {
       return modulo(num1, num2);
       break;
   }
+}
+
+function callOperation() {
+  screenTop.textContent = operate(
+    operator,
+    screenTop.textContent,
+    screenBottom.textContent
+  );
+  screenBottom.textContent = screenTop.textContent;
+  operator = "";
 }
